39df37a00d050a643861950f726c5fa721bb2da6
Praatibh Surana <43675042+praatibhsurana@users.noreply.github.com>
2022-01-19 14:07:08 +0530

    Added twisted module filepath

--- src/hinglish-hindi-wsd/hipos.py
+++ src/hinglish-hindi-wsd/hipos.py
@@ -1,32 +1,36 @@
 from polyglot_tokenizer import Tokenizer
-from indic_tagger.tagger.src.algorithm.CRF import CRF
+from hindiwsd.CRF import CRF 
 from sklearn.model_selection import train_test_split
 from time import time
 import numpy as np
 import pickle
 import logging
 import argparse
-import indic_tagger.tagger.utils.writer as data_writer
-import indic_tagger.tagger.src.generate_features as generate_features
-import indic_tagger.tagger.src.data_reader as data_reader
-# import lstmcrf
-from indic_tagger.lstmcrf.utils import load_data_and_labels
-from indic_tagger.lstmcrf.wrapper import Sequence
+import hindiwsd.writer as data_writer
+from hindiwsd import generate_features, data_reader 
+#import lstmcrf
+from hindiwsd.utils import load_data_and_labels
+from hindiwsd.wrapper import Sequence
 import sys
 import os.path as path
 import os
 sys.path.append(path.dirname(path.abspath(__file__)))
 
+from twisted.python.modules import getModule
+
+moduleDirectory = getModule("hindiwsd").filePath.parent()
+crf_pos_model = str(moduleDirectory.child("crf.pos.utf.model"))[10:-2]
+
 
 def pos_pipeline(sentence):
 
-    f = open('./src/test.txt', 'w', encoding='utf8')
+    f = open('./test.txt', 'w', encoding='utf8')
     f.write(sentence)
     f.close()
 
     curr_dir = path.dirname(path.abspath(__file__))
     args = {'pipeline_type': 'predict', 'language': 'hi', 'tag_type': 'pos',
-            'model_type': 'crf', 'encoding': 'utf', 'data_format': 'txt', 'sent_split': True, 'test_data': './src/test.txt', 'output_path': './src/out.txt'}
+            'model_type': 'crf', 'encoding': 'utf', 'data_format': 'txt', 'sent_split': True, 'test_data': './test.txt', 'output_path': './out.txt'}
 
     output_dir = path.join(path.dirname(path.abspath(__file__)), "outputs")
     # if not os.path.exists(output_dir):
@@ -67,7 +71,7 @@ def pos_pipeline(sentence):
                 curr_dir, args['language'], args['model_type'], "chunk", args['encoding'])
 
             if args['model_type'] == "crf":
-                tagger = CRF(tag_model_path)
+                tagger = CRF(crf_pos_model)
                 tagger.load_model()
                 y_pos = tagger.predict(X_test)
 
@@ -91,27 +95,14 @@ def pos_pipeline(sentence):
                 s, args['tag_type'], args['model_type']) for s in test_sents]
 
             if args['model_type'] == "crf":
-                tagger = CRF(model_path)
+                tagger = CRF(crf_pos_model)
                 tagger.load_model()
                 y_pred = tagger.predict(X_test)
-                print("Y_PRED:", y_pred)
+                # print("Y_PRED:", y_pred)
                 data_writer.write_anno_to_file(
                     args['output_path'], test_sents, y_pred, args['tag_type'])
                 tagged = data_writer.write_to_screen(args['output_path'])
                 # logger.info("Output in: %s" % args.output_path)
-
-            # if args['model_type'] == "lstm":
-            #     model = Sequence().load(model_path+"/weights.h5", model_path +
-            #                             "/params.json", model_path+"/preprocessor.json")
-            #     f = open(args['test_data'], "r")
-            #     sent = f.read()
-            #     tok = Tokenizer(lang=args['language'], split_sen=True)
-            #     tokenized_sents = tok.tokenize(sent)
-            #     for tokens in tokenized_sents:
-            #         for token in tokens:
-            #             sent = sent + " " + token
-            #         sent = sent.strip()
-            #         print(model.analyze(sent))
 
     return tagged.split()
 
